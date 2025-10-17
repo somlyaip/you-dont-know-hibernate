7111354e04a106f8d271b8870d407e452dbf324f
Pál Somlyai <somlyai.pal@gmail.com>
2025-10-17 08:56:52 +0200

    build: revert lombok and standardize java version configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
 group = "dev.somlyaip.blog"
 version = "0.0.1-SNAPSHOT"
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
 plugins {
     java
     id("org.springframework.boot") apply false
